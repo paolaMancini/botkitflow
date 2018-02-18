94542ae29d3e65c7d7df546dbb6235c5039cfc1b
paolaMancini <paola.mancini@italtel.com>
2018-02-18 09:31:24 +0100

    Update plantPerformance.js

--- skills/plantPerformance.js
+++ skills/plantPerformance.js
@@ -28,37 +28,62 @@ module.exports = function (controller) {
 	       
 	                    
 	                   
-	                   bot.startConversation(message, function (err, convo) {
+	                  var lines= ["fakeMachine0","fakeMachine1","fakeMachine2","fakeMachine3","fakeMachine4","fakeMachine5","fakeMachine6"];
+		
+		 
+				bot.startConversation(message, function (err, convo) {
 	
-	                   convo.ask("Do you want more details?", 
-	                            [
-	                           { pattern: "^yes|yep|y|Yes$",
-	                               callback: function (response, convo) {
-	                                  convo.gotoThread("yes");
-	                               },
-	                           },
-	                           { pattern: "^no|No|n$",
-	                               callback: function (response, convo) {
-	                                  convo.gotoThread("no");
-	                               },
-	                           },
-	                           {
-	                               default: true,
-	                               callback: function (response, convo) {
-	                                  convo.say("Sorry, I don't know this color. Try another one...");
-	                                  convo.repeat();
-	                                  convo.next();
-	                               }
-	                           }], { key: "answer" });
+				// create a path for when a user says YES
+				convo.addMessage({
+					text: 'How wonderful.',
+				}, 'yes_thread');
 	
-	                         // yes thread
-	                         convo.addMessage(
-	                           "Cool, I love '{{responses.answer}}' too",
-	                           "yes");
-	                           
-	                         convo.addMessage(
-	                           "Glad having being helped you",
-	                           "no");
-	                     })  
-	               } );
+				// create a path for when a user says NO
+				// mark the conversation as unsuccessful at the end
+				convo.addMessage({
+					text: 'Glad having being helped you',
+					action: 'stop', // this marks the converation as
+									// unsuccessful
+				}, 'no_thread');
+	
+				// create a path where neither option was matched
+				// this message has an action field, which directs botkit to
+				// go back to the `default` thread after sending this
+				// message.
+				convo.addMessage({
+					text: 'Sorry I did not understand. Plese choose the line: fakeMachine0,fakeMachine1,fakeMachine2,fakeMachine3,fakeMachine4,fakeMachine5,fakeMachine6',
+					action: 'default',
+			    }, 'bad_response');
+	
+				// Create a yes/no question in the default thread...
+				convo.ask('Which line are you interested of?', [{
+						 pattern:'fakeMachine0|fakeMachine1|fakeMachine2|fakeMachine3|fakeMachine4|fakeMachine5|fakeMachine6',
+						 callback: function (response, convo) {
+							 convo.gotoThread('yes_thread');
+						 },	
+				},
+				{
+						pattern: 'no-line',
+						callback: function (response, convo) {
+							convo.gotoThread('no_thread');
+						},
+				},
+				{
+						default: true,
+						callback: function (response, convo) {
+							convo.gotoThread('bad_response');
+						},
+					
+				}]);
+	
+				// capture the results of the conversation and see what
+				// happened...
+				convo.on('end', function (convo) {
+	
+					if (convo.successful()) {
+					 	// this still works to send individual replies...
+						bot.reply(message, 'Let us eat some!');
+					}					
+				});
+			 });
 	           }
