d5bbbb0062a66e6f471b64a323c2e68d203472d6
paolaMancini <paola.mancini@italtel.com>
2018-02-19 00:21:09 +0100

    Update linePerformance.js

--- skills/linePerformance.js
+++ skills/linePerformance.js
@@ -2,7 +2,8 @@ module.exports = function (controller) {
 	
 	// controller.hears(['cheese'], 'direct_message,direct_mention',
 	// function (bot, message) {
-	controller.hears([/line ([a-zA-Z0-9]{1,}) performance], 'direct_message,direct_mention', function (bot, message) {
+	//controller.hears([/line ([a-zA-Z0-9]{1,}) performance], 'direct_message,direct_mention', function (bot, message) {
+	controller.hears([/line performance/, 'direct_message,direct_mention', function (bot, message) {
 		
 		var lines= ["fakeMachine0","fakeMachine1","fakeMachine2","fakeMachine3","fakeMachine4","fakeMachine5","fakeMachine6"];
 		
