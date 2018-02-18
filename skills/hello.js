c0a8c5c54d0ff4c2d8f721a3411a2bd11a2263b6
paolaMancini <paola.mancini@italtel.com>
2018-02-19 00:57:12 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,10 +1,11 @@
 //
 // Fallback Command
 //
+var dt = require('./restClient');
 module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
-
+        cosole.log("The date and time are currently: " + dt.myDateTime());
         bot.reply(message, "Hello");
              
     });
