55e634f8b09cca10c9fde030133647c3a81c27c6
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:04:30 +0100

    Update restClient.js

--- skills/restClient.js
+++ skills/restClient.js
@@ -1,4 +1,21 @@
-exports.myDateTime = function () {
-    return Date();
-};
-	       
+//exports.myDateTime = function () {
+  //  return Date();
+//};
+
+ var request = require('request');	       
+ exports.allData=function () {
+	 request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
+                console.log('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+
+                var jsonData = JSON.parse(body);
+                for (var i = 0; i < jsonData.length; i++) {
+                    var counter = jsonData.machines[i];
+                    console.log(counter.machine.oee);
+                }
+		 return jsonData;
+            });
+ }
+
+            
