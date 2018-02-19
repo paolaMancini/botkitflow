e3a595573fb39a9470f5d7da615e58b6f415e168
paolaMancini <paola.mancini@italtel.com>
2018-02-19 11:44:00 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -6,7 +6,7 @@ var smartfab = require('./restClient.js');
 module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
-        console.log("smartfab.allData.lenght: "+smartfab.allData.lenght);
+        console.log("smartfab.machines.lenght: "+smartfab.machines.lenght);
         //console.log("The date and time are currently: " + dt.myDateTime())       
         bot.reply(message, "Hello");
              
