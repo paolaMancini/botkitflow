b2f323ca3896e4c06320a455f20852eea3df2936
paolaMancini <paola.mancini@italtel.com>
2018-02-19 12:18:05 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -2,11 +2,23 @@
 // Fallback Command
 //
 //var smartfab = require('./restClient.js');
-var  = require("./events.js");
+var smartfab = require("./events.js");
+var request = require("request");
+
 module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
-        
+
+        var options = { method: 'GET',
+             url: 'http://194.79.57.109:8080//SFapi/machines',
+         };
+
+        request(options, function (error, response, body) {
+         if (error) throw new Error(error);
+
+             console.log(body);
+        });
+
         smartfab.machines("fakeMachine0",function (err, events, text) {a
         if (err) {
                  console.log("#### ERROR");
