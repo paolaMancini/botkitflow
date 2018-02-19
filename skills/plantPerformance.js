0dd964fe254b364c4a64591fbf7dec6cd5c475e5
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:42:24 +0100

    Update plantPerformance.js

--- skills/plantPerformance.js
+++ skills/plantPerformance.js
@@ -6,24 +6,31 @@ module.exports = function (controller) {
 	             console.log('message: ',message);
 	              
 		var url='http://194.79.57.109:8080/SFapi/machines';		
-        request(url, function(error, response, body) {
+        	request(url, function(error, response, body) {
                        console.log('error:', error); // Print the error if one occurred
                        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
                        console.log('body:', body); // Print the HTML for the Google homepage.
        
                        var jsonData = JSON.parse(body);
-						 
+			var macs=[];
+			var aliases=[];	
+			var OEEs=[];	
                        for (var i = 0; i < jsonData.machines.length; i++) {
-							var machine = jsonData.machines[i].machine;
-							var alias = jsonData.machines[i].alias;
-							var oee = jsonData.machines[i].alias;
-						
-							console.log(machine);
-							console.log(alias);
-							console.log(oee);
+				var machine = jsonData.machines[i].machine;
+			        
+				var alias = jsonData.machines[i].alias;
+				var oee = jsonData.machines[i].alias;
+				console.log(machine);
+				console.log(alias);
+				console.log(oee);
+			        macs.push(machine);
+			         aliases.push(alias);
+			         OEEs.push(oee);
                       }
                    
-                 
+                 	console.log('macs: '+macs.join("|"));
+			console.log('aliases: '+aliases.join(","));
+			console.log('OEEs: '+OEEs.join(","));
 	       
 	                    
 	                   
