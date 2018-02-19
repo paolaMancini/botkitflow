a3cb5ebd5fbfd99bfd57acf7b38308f64954de94
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:29:08 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,20 +1,31 @@
 //
 // Fallback Command
 //
-var dt = require('./restClient');
+//var dt = require('./restClient');
 module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
         //console.log("The date and time are currently: " + dt.myDateTime());
-         for (var i = 0; i < 7; i++) {
-                           var machine = dt.allData.machines[i].machine;
-                         var alias =dt.allData.machines[i].alias;
-                         var oee = dt.allData.machines[i].alias;
+        request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
+                       console.log('error:', error); // Print the error if one occurred
+                       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                       console.log('body:', body); // Print the HTML for the Google homepage.
+       
+                       var jsonData = JSON.parse(body);
+                     //var linea0 = jsonData.machines[0].machine;
+                     //console.log('linea0:' ,linea0); 
+                       for (var i = 0; i < jsonData.machines.length; i++) {
+                           var machine = jsonData.machines[i].machine;
+                         var alias = jsonData.machines[i].alias;
+                         var oee = jsonData.machines[i].alias;
                      
                            console.log(machine);
                          console.log(alias);
                          console.log(oee);
-         }
+                      }
+                     return jsonData;
+                   });
+          
         bot.reply(message, "Hello");
              
     });
