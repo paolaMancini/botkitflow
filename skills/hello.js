55dc9bfbda03bc6ca568c47ecaad5d4c53cc1e93
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:06:50 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -6,6 +6,7 @@ module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
         console.log("The date and time are currently: " + dt.myDateTime());
+        console.log("jsonData= "+dt.allData);
         bot.reply(message, "Hello");
              
     });
