1956ca2d0f9469e40a60ae2030b3a7ef9dec4ce8
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:35:57 +0100

    Update plantPerformance.js

--- skills/plantPerformance.js
+++ skills/plantPerformance.js
@@ -1,31 +1,29 @@
+var request = require('request');
 module.exports = function (controller) {
 	             // var request = require('request');
 	 	     //controller.hears( [/(plant)( [a-zA-Z0-9]{1,})|(plants)|(performance)/], 'direct_message,direct_mention', function(bot, message){
 	             controller.hears(/plant1 performance/i, 'direct_message,direct_mention', function(bot, message){
 	             console.log('message: ',message);
 	              
-	                  
-// request('http://194.79.57.109:8080/SFapi/machines', function(error, response,
-// body) {
-// console.log('error:', error); // Print the error
-// // if one occurred
-// console.log('statusCode:', response && response.statusCode);
-// console.log('body:', body); // Print the HTML for
-// // the Google homepage.
-//	       
-// var jsonData = JSON.parse(body);
-// // var linea0 = jsonData.machines[0].machine;
-// // console.log('linea0:' ,linea0);
-// for (var i = 0; i < jsonData.machines.length; i++) {
-// var machine = jsonData.machines[i].machine;
-// var alias = jsonData.machines[i].alias;
-// var oee = jsonData.machines[i].alias;
-//	                     
-// console.log(machine);
-// console.log(alias);
-// console.log(oee);
-// }
-// });
+		var url='http://194.79.57.109:8080/SFapi/machines';		
+        request(url, function(error, response, body) {
+                       console.log('error:', error); // Print the error if one occurred
+                       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                       console.log('body:', body); // Print the HTML for the Google homepage.
+       
+                       var jsonData = JSON.parse(body);
+						 
+                       for (var i = 0; i < jsonData.machines.length; i++) {
+							var machine = jsonData.machines[i].machine;
+							var alias = jsonData.machines[i].alias;
+							var oee = jsonData.machines[i].alias;
+						
+							console.log(machine);
+							console.log(alias);
+							console.log(oee);
+                      }
+                     return jsonData;
+                 
 	       
 	                    
 	                   
@@ -86,6 +84,7 @@ module.exports = function (controller) {
 						bot.reply(message, 'Let us eat some!');
 					}					
 				});
-			 });
-	           })
+			}); 
+		});
+	})
 }
