84c66873a786d141f1740bb601f83b39dd1e72f3
paolaMancini <paola.mancini@italtel.com>
2018-02-19 01:26:06 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -6,7 +6,15 @@ module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
         //console.log("The date and time are currently: " + dt.myDateTime());
-        console.log("jsonData= "+dt.router);
+         for (var i = 0; i < dt.allData.machines.length; i++) {
+                           var machine = dt.allData.machines[i].machine;
+                         var alias =dt.allData.machines[i].alias;
+                         var oee = dt.allData.machines[i].alias;
+                     
+                           console.log(machine);
+                         console.log(alias);
+                         console.log(oee);
+         }
         bot.reply(message, "Hello");
              
     });
