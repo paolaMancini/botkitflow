b6b157254d0b5abce91d1febda0641ec7dd05e00
paolaMancini <paola.mancini@italtel.com>
2018-02-19 10:42:08 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,11 +1,12 @@
 //
 // Fallback Command
 //
-//var dt = require('./restClient');
+var smartfab = require('./restClient');
 
 module.exports = function (controller) {
 
     controller.hears(['hello|hi|good morning'], 'direct_message,direct_mention', function (bot, message) {
+        console.log("smartfab.allData.lenght: "+smartfab.allData.lenght);
         //console.log("The date and time are currently: " + dt.myDateTime())       
         bot.reply(message, "Hello");
              
