8b2067dbf4a55d8e220b16d58392e03332092846
mancinip <paola.mancini@italtel.com>
2018-02-20 11:37:47 +0100

    Deleted 'about' from pattern

--- skills/aboutWho.js
+++ skills/aboutWho.js
@@ -1,12 +1,12 @@
 //
 // Fallback Command
 //
-module.exports = function (controller) {
+module.exports = function(controller) {
 
-    controller.hears(['about|who are you'], 'direct_message,direct_mention', function (bot, message) {
-        var mardown = "This is the IndychatBot for ROLD SmartFab, the ready-to-use platform which allows the SMEs to monitor, analyze and manage data and information coming from production plants. To know which information I provide, please type:"+ "\n- "
-            + bot.appendMention(message, "help");
-            
+    controller.hears(['who are you'], 'direct_message,direct_mention', function(bot, message) {
+        var mardown = "This is the IndychatBot for ROLD SmartFab, the ready-to-use platform which allows the SMEs to monitor, analyze and manage data and information coming from production plants. To know which information I provide, please type:" +
+            bot.appendMention(message, "help");
+
         bot.reply(message, mardown);
     });
 }
