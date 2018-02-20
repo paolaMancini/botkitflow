17f8ac1f4cc0c298bfaa29a86cecd874a823adb7
mancinip <paola.mancini@italtel.com>
2018-02-20 10:06:30 +0100

    Modified pattern

--- skills/hello.js
+++ skills/hello.js
@@ -1,14 +1,16 @@
 module.exports = function(controller) {
 
-    controller.hears(/^hi|hello|goodmorning$'/gi, 'message_received', function(bot, message) {
-
+    controller.hears(['hello', 'hey', 'hi', 'aloha'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         var message_options = [
             "Hello there!",
-            "Hi!"
+            "Hello.",
+            "Hi.",
+            "Hey, what's up!",
         ]
         var random_index = Math.floor(Math.random() * message_options.length)
         var chosen_message = message_options[random_index]
 
         bot.reply(message, chosen_message)
+
     });
 }
