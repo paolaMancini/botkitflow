6f4b9a9004feacfe9ad606f29fe7c1f307e0b2b4
mancinip <paola.mancini@italtel.com>
2018-02-20 13:58:39 +0100

    Bug solved

--- skills/hello.js
+++ skills/hello.js
@@ -1,21 +1,17 @@
 module.exports = function(controller) {
 
-        controller.hears(['hello', 'hey', 'aloha'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-                var message_options = [
-                    "Hello there!",
-                    "Hello.",
-                    "Hi.",
-                    "Hey, what's up!",
-                ]
-                var random_index = Math.floor(Math.random() * message_options.length)
-                var chosen_message = message_options[random_index]
+    controller.hears(['hello', 'hey', 'aloha'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        var message_options = [
+            "Hello there!",
+            "Hello.",
+            "Hi.",
+            "Hey, what's up!",
+        ]
+        var random_index = Math.floor(Math.random() * message_options.length)
+        var chosen_message = message_options[random_index]
+        bot.say('```[📞](Contact Center: //pmr?sip=sip:paola.mancini@italtel.call.ciscospark.com)```');
 
-                bot.reply(message, chosen_message)
-                bot.reply(message, '
-                    ``
-                    `[📞](Contact Center: //pmr?sip=sip:paola.mancini@italtel.call.ciscospark.com)`
-                    ``
-                    ');
+    });
+    bot.reply(message, chosen_message)
 
-                });
-        }
+}
