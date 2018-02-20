32c2661c9fa6db30bb7693d6edb5973794788c0a
mancinip <paola.mancini@italtel.com>
2018-02-20 09:45:44 +0100

    Modified

--- skills/hello.js
+++ skills/hello.js
@@ -1,12 +1,14 @@
 module.exports = function(controller) {
 
-    controller.hears('hello' | 'hi' | 'good morning', 'message_received', function(bot, message) {
+    controller.hears(['hi', 'hello', 'good morning'], 'message_received', function(bot, message) {
 
-        if (message.user && message.user.name) {
-            bot.reply(message, 'Hello ' + user.name + '!!');
-        } else {
-            bot.reply(message, 'Hello.');
-        }
+        var message_options = [
+            "Hello there!",
+            "Hi!"
+        ]
+        var random_index = Math.floor(Math.random() * message_options.length)
+        var chosen_message = message_options[random_index]
 
+        bot.reply(message, chosen_message)
     });
 }
