49840c3ac7a68ed66362c4150bc873d5405148ee
mancinip <paola.mancini@italtel.com>
2018-02-20 09:41:40 +0100

    Modified pattern

--- skills/hello.js
+++ skills/hello.js
@@ -1,6 +1,6 @@
 module.exports = function(controller) {
 
-    controller.hears([/'hello'|'hi'|'good morning'/], 'message_received', function(bot, message) {
+    controller.hears('hello' | 'hi' | 'good morning', 'message_received', function(bot, message) {
 
         if (message.user && message.user.name) {
             bot.reply(message, 'Hello ' + user.name + '!!');
