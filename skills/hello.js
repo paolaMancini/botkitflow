4f7645247a3059e19c8b79c4b31ead6440e6610f
mancinip <paola.mancini@italtel.com>
2018-02-20 13:55:35 +0100

    Added markup language

--- skills/hello.js
+++ skills/hello.js
@@ -1,16 +1,21 @@
 module.exports = function(controller) {
 
-    controller.hears(['hello', 'hey', 'aloha'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        var message_options = [
-            "Hello there!",
-            "Hello.",
-            "Hi.",
-            "Hey, what's up!",
-        ]
-        var random_index = Math.floor(Math.random() * message_options.length)
-        var chosen_message = message_options[random_index]
+        controller.hears(['hello', 'hey', 'aloha'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+                var message_options = [
+                    "Hello there!",
+                    "Hello.",
+                    "Hi.",
+                    "Hey, what's up!",
+                ]
+                var random_index = Math.floor(Math.random() * message_options.length)
+                var chosen_message = message_options[random_index]
 
-        bot.reply(message, chosen_message)
+                bot.reply(message, chosen_message)
+                bot.reply(message, '
+                    ``
+                    `[📞](Contact Center: //pmr?sip=sip:paola.mancini@italtel.call.ciscospark.com)`
+                    ``
+                    ');
 
-    });
-}
+                });
+        }
