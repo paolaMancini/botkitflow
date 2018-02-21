b1f5663c555091b487ff3688a1fe97aa00e22051
mancinip <paola.mancini@italtel.com>
2018-02-21 02:47:23 +0100

    Solved bug

--- skills/events.js
+++ skills/events.js
@@ -1,50 +1,104 @@
 var debug = require("debug")("samples");
 var fine = require("debug")("samples:fine");
-module.exports.fetchCurrent = function(cb) {
-    var request = require("request");
-    // Get list of upcoming events
-    var options = {
-        method: 'GET',
-        url: "http://194.79.57.109:8080//SFapi/machines"
-    };
 
-    request(options, function(error, response, body) {
-        if (error) {
-            debug("1 could not retreive list of events, error: " + error);
-            //cb(new Error("Could not retreive current events, sorry [Events API not responding]"), null, null);
-            return;
+module.exports.fetchMachines = function(cb) {
+        var request = require("request");
+        // Get list of upcoming events
+        var options = {
+            method: 'GET',
+            url: "http://194.79.57.109:8080//SFapi/machines"
+        };
+
+        request(options, function(error, response, body) {
+            if (error) {
+                debug("1 could not retreive list of events, error: " + error);
+                //cb(new Error("Could not retreive current events, sorry [Events API not responding]"), null, null);
+                return;
+            }
+
+            if ((response < 200) || (response > 299)) {
+                debug("1 could not retreive list of events, response: " + response);
+                //sparkCallback(new Error("Could not retreive current events, sorry [bad anwser from Events API]"), null, null);
+                return;
+            }
+
+            var events = JSON.parse(body);
+            debug("fetched " + events.machines.length + " events");
+            fine(JSON.stringify(events));
+
+            if (events.length == 0) {
+                cb(null, events, "**Found no event currently going on.**");
+                return;
+            }
+
+            var nb = events.length;
+            var msg = "**" + nb + " events are running now:**";
+            if (nb == 1) {
+                msg = "**only one event is running now:**";
+            }
+            for (var i = 0; i < nb; i++) {
+                var current = events[i];
+                //msg += "\n:small_blue_diamond: "
+                msg += "\n" + (i + 1) + ". ";
+                msg += current.machine + " - " + current.description + +" - " + current.value;
+                debug("msg= ", msg);
+            }
+
+
+
+            cb(null, events, msg);
+        });
+
+        module.exports.fetchMachDetails = function(machine, cb) {
+            var request = require("request");
+            // Get list of upcoming events
+            var options = {
+                method: 'GET',
+                url: "http://194.79.57.109:8080/SFapi//status?machine=" + machine
+            };
+
+            request(options, function(error, response, body) {
+                if (error) {
+                    debug("1 could not retreive list of events, error: " + error);
+                    //cb(new Error("Could not retreive current events, sorry [Events API not responding]"), null, null);
+                    return;
+                }
+
+                if ((response < 200) || (response > 299)) {
+                    debug("1 could not retreive list of events, response: " + response);
+                    //sparkCallback(new Error("Could not retreive current events, sorry [bad anwser from Events API]"), null, null);
+                    return;
+                }
+
+                var events = JSON.parse(body);
+                debug("fetched " + events.machine.length + " events");
+                fine(JSON.stringify(events));
+
+                if (events.machine.length == 0) {
+                    cb(null, events, "**Found no event currently going on.**");
+                    return;
+                }
+
+                var nb = events.machine.length;
+                var msg = "Details:<br>";
+
+                //if (nb == 1) {
+                //  msg = "**only one event is running now:**";
+                //}
+                for (var i = 0; i < nb; i++) {
+                    var current = events[i];
+                    //msg += "\n:small_blue_diamond: "
+                    if (i > 0) {
+                        msg += ";<br" >
+                    }
+                    msg += current.name + " - " + current.description + +" - " + current.value;
+                    msg += current.description + ": **" + current.value + " **<br>";
+                    debug("msg= ", msg);
+
+                }
+
+
+
+                cb(null, events, msg);
+            });
         }
-
-        if ((response < 200) || (response > 299)) {
-            debug("1 could not retreive list of events, response: " + response);
-            //sparkCallback(new Error("Could not retreive current events, sorry [bad anwser from Events API]"), null, null);
-            return;
-        }
-
-        var events = JSON.parse(body);
-        debug("fetched " + events.machines.length + " events");
-        fine(JSON.stringify(events));
-
-        if (events.length == 0) {
-            cb(null, events, "**Found no event currently going on.**");
-            return;
-        }
-
-        var nb = events.length;
-        var msg = "**" + nb + " events are running now:**";
-        if (nb == 1) {
-            msg = "**only one event is running now:**";
-        }
-        for (var i = 0; i < nb; i++) {
-            var current = events[i];
-            //msg += "\n:small_blue_diamond: "
-            msg += "\n" + (i + 1) + ". ";
-            msg += current.machine + " - " + current.description + +" - " + current.value;
-            debug("msg= ", msg);
-        }
-
-
-
-        cb(null, events, msg);
-    });
-}
