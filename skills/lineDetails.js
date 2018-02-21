ff523a6f8c1a4c8cdaa492c2a39f8b282c27dd43
mancinip <paola.mancini@italtel.com>
2018-02-21 02:47:39 +0100

    Modified rest call

--- skills/lineDetails.js
+++ skills/lineDetails.js
@@ -5,9 +5,12 @@ module.exports = function(controller) {
     // controller.hears(['cheese'], 'direct_message,direct_mention',
     // function (bot, message) {
     controller.hears([/(.*) details/i], 'direct_message,direct_mention', function(bot, message) {
+        console.log('message: ', message);
+        var lineName = message.match[1];
+        console.log("lineName received: ", lineName);
 
-        console.log("inizio");
-        Events.fetchCurrent(function(err, events, text) {
+
+        Events.fetchMachDetails(lineName, function(err, events, text) {
             if (err) {
                 bot.reply(message, "*sorry, could not contact the organizers :-(*");
                 return;
@@ -21,44 +24,38 @@ module.exports = function(controller) {
             // Store events
             console.log("text: ", text);
 
-        });
-        console.log("fine");
-        console.log('message: ', message);
-        var lineName = message.match[1];
-        console.log("lineName received: ", lineName);
+            //  });
 
-        console.log('message: ', message);
-        var plantName = message.match[1];
-        //match[1] is the (.*) group. match[0] is the entire group (open the (.*) doors).
 
-        var url = 'http://194.79.57.109:8080/SFapi/status?machine=' + lineName;
-        console.log("url:  ", url);
 
-        request(url, function(error, response, body) {
-            console.log('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
+            var url = 'http://194.79.57.109:8080/SFapi/status?machine=' + lineName;
+            console.log("url:  ", url);
 
-            if (error) {
-                console.log("could not retreive list of events, error: " + error);
-                bot.reply(message, "The machine is not responding");
-                return;
-            }
+            //  request(url, function(error, response, body) {
+            //    console.log('error:', error); // Print the error if one occurred
+            //    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+            //  console.log('body:', body); // Print the HTML for the Google homepage.
 
-            if ((response < 200) || (response > 299)) {
-                console.log("could not retreive list of events, response: " + response);
-                bot.reply(message, "The machine is not responding");
-                return;
-            }
-            if (body.length == 0) {
-                console.log("body is null");
-                bot.reply(message, "The machine is not responding");
-                return;
-            }
-            var jsonData = JSON.parse(body);
+            //   if (error) {
+            //      console.log("could not retreive list of events, error: " + error);
+            //       bot.reply(message, "The machine is not responding");
+            //     return;
+            //  }
 
-            var textDef = "Details:<br>";
-            var text = textDef;
+            //  if ((response < 200) || (response > 299)) {
+            //       console.log("could not retreive list of events, response: " + response);
+            //     bot.reply(message, "The machine is not responding");
+            //      return;
+            // }
+            // if (body.length == 0) {
+            //     console.log("body is null");
+            //  bot.reply(message, "The machine is not responding");
+            //  return;
+            //}
+            //var jsonData = JSON.parse(body);
+
+            // var textDef = "Details:<br>";
+            //var text = textDef;
 
             for (var i = 0; i < jsonData.machine.length; i++) {
                 var name = jsonData.machine[i].machine;
@@ -67,7 +64,7 @@ module.exports = function(controller) {
                 console.log('name: ', name);
                 console.log('description: ', descr);
                 console.log('value: ', value);
-                text += descr + ": **" + value + " **;<br><br>";
+                text += descr + ": **" + value + " **;<br>";
 
             }
             if (text === textDef) {
