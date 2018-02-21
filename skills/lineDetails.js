51fb2f534b7990d37309d6c2fcbb8b6204f80448
paolaMancini <paola.mancini@italtel.com>
2018-02-21 09:02:03 +0100

    Update lineDetails.js

--- skills/lineDetails.js
+++ skills/lineDetails.js
@@ -24,54 +24,64 @@ module.exports = function(controller) {
 
             // Store events
             console.log("text: ", text);
-
-            // });
-
+            bot.say(message, text);
 
 
-            //  var url = 'http://194.79.57.109:8080/SFapi/status?machine=' + lineName;
-            //  console.log("url:  ", url);
+            bot.startConversation(message, function(err, convo) {
 
-            //  request(url, function(error, response, body) {
-            //    console.log('error:', error); // Print the error if one occurred
-            //    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            //    console.log('body:', body); // Print the HTML for the Google homepage.
+                // create a path for when a user says YES
+                var help = "Which line are you interested of? Please, type<br>" + mpattern;
+                help += detailMsg;
 
-            //   if (error) {
-            //       console.log("could not retreive list of events, error: " + error);
-            //      bot.reply(message, "The machine is not responding");
-            //      return;
-            //  }
+                convo.addMessage({
+                    text: `_${help}_`,
+                }, 'ask-other');
 
-            //  if ((response < 200) || (response > 299)) {
-            //       console.log("could not retreive list of events, response: " + response);
-            //      bot.reply(message, "The machine is not responding");
-            //      return;
-            //  }
-            //  if (body.length == 0) {
-            //     console.log("body is null");
-            //     bot.reply(message, "The machine is not responding");
-            //     return;
-            //  }
-            //  var jsonData = JSON.parse(body);
+                // create a path where neither option was matched
+                // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+                convo.addMessage({
+                    text: 'Sorry I did not understand. Say `yes` or `no`',
+                    action: 'default',
+                }, 'bad_response');
 
-            // var textDef = "Details:<br>";
-            //   var text = textDef;
 
-            //  for (var i = 0; i < jsonData.machine.length; i++) {
-            //    var name = jsonData.machine[i].machine;
-            //    var descr = jsonData.machine[i].description;
-            //    var value = jsonData.machine[i].value;
-            //     console.log('name: ', name);
-            //     console.log('description: ', descr);
-            //     console.log('value: ', value);
-            //   text += descr + ": **" + value + " **;<br>";
+                convo.say("The performance data is:<br>" + oees);
+                convo.ask("Are you interested on  monitoring an other further line? (yes/**no**/cancel)", [{
+                        pattern: "yes|yeh|sure|oui|si",
+                        callback: function(response, convo) {
+                            convo.gotoThread('ask-other');
+                        },
+                    },
+                    {
+                        pattern: "no|neh|non|na|birk",
+                        callback: function(response, convo) {
+                            convo.say("Glad have being helped you!");
+                            convo.next();
 
-            // }
-            //  if (text === textDef) {
+                        },
+                    },
+                    {
+                        pattern: "cancel|stop|exit",
+                        callback: function(response, convo) {
+                            convo.say("Got it, cancelling...");
+                            convo.next();
+                        },
+                    },
+                    {
+                        default: true,
+                        callback: function(response, convo) {
+                            convo.say("Sorry, I did not understand.");
+                            convo.repeat();
+                            convo.next();
+                        }
+                    },
+                ]);
 
-            //  }
-            bot.reply(message, text);
+
+            });
+
+
+
 
         });
     })
