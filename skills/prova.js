88dfcb30f446ad653d0e57dfa4d516749dad83e3
paolaMancini <paola.mancini@italtel.com>
2018-02-22 08:49:50 +0100

    Update prova.js

--- skills/prova.js
+++ skills/prova.js
@@ -3,7 +3,7 @@ module.exports = function(controller) {
 
     controller.hears(['prova'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         
-        var chosen_message = '[📞](Contact Center: //pmr?sip=sip:paola.mancini@italtel.call.ciscospark.com)' ;
+        var chosen_message = 'http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine0&graph=2&graphPage=0' ;
        
         bot.reply(message, chosen_message);
         bot.reply();
