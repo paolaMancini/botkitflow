569b416a2a228da4ea826a07b91f9efba10996c2
paolaMancini <paola.mancini@italtel.com>
2018-02-22 13:09:50 +0100

    Update plantMonitored.js

--- skills/plantMonitored.js
+++ skills/plantMonitored.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
 
-    controller.hears(['plants moitored'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-         bot.reply(message, "The plants monitored are:<br>**Plant1**");
+    controller.hears(['plants monitored'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+         bot.reply(message, "The plants monitored are:<br>**plant1**");
     });
 
 
