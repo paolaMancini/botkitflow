ec58a2b0c3faab805151d04b60daa07b5984598f
paolaMancini <paola.mancini@italtel.com>
2018-02-22 16:42:18 +0100

    Update prova.js

--- skills/prova.js
+++ skills/prova.js
@@ -4,6 +4,7 @@ module.exports = function(controller) {
     controller.hears(['web portal'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         
         var chosen_message = '[ROLD SmartFab web portal!](http://194.79.57.109/smartFabDEMO/)';
+        bot.reply(message, "Have a look to the ");
         bot.reply(message, chosen_message);
         bot.reply();
 
