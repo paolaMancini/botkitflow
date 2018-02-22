99983879dcddc6324337e2c691ac204b9db6bb95
paolaMancini <paola.mancini@italtel.com>
2018-02-23 00:14:33 +0100

    Update stuff.js

--- skills/stuff.js
+++ skills/stuff.js
@@ -1,6 +1,6 @@
 module.exports = function(controller) {
 
-    controller.hears(['how are you'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears([/how are you/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         var message_options = [
             "Fine thanks!",
             "Very well, thank you.",
