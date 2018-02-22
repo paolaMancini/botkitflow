a5b22b46061816ee3a4c3cba6739474a4f9f0f26
paolaMancini <paola.mancini@italtel.com>
2018-02-22 15:06:36 +0100

    Update lineAvailability.js

--- skills/lineAvailability.js
+++ skills/lineAvailability.js
@@ -12,12 +12,12 @@ module.exports = function(controller) {
  
         Events.fetchMachines(function(err, plant, text) {
             if (err) {
-                bot.reply(message, "*sorry, could not contact the organizers :-(*");
+                 bot.reply(message, "The machine is not responding");
                 return;
             }
 
             if (plant.length == 0) {
-                bot.reply(message, text + "\n\n_Type next for upcoming events_");
+               bot.reply(message, "The machine is not responding");
                 return;
             }
 
@@ -46,36 +46,28 @@ module.exports = function(controller) {
             } else {
 
                 console.log('machineName: ', machineName);
+             
+             
+                        Events.fetchMachDetails1(machineName,lineName,param,function(errMach, events, textMach) {
+                            if (errMach) {
+                                bot.reply(message, "The machine is not responding");
+                                return;
+                            }
 
-                Events.fetchMachDetails(machineName, function(errMach, events, textMach) {
-                    if (errMach) {
-                        bot.reply(message, "*sorry, could not contact the organizers :-(*");
-                        return;
-                    }
-
-                    if (events.length == 0) {
-                        bot.reply(message, textMach + "\n\n_Type next for upcoming events_");
-                        return;
-                    }
-                    var mex;
-                    for (var i = 0; i < events.machine.length; i++) {
-                        var current = events.machine[i];
-
-                        if (events.machine[i].name == "availability") {
-
-                            mex=current.description + ": **" + current.value + "**";
-                        }
-                      
-
-                    }
-
-                    // Store events
-                    console.log("text: ", mex);
-                    bot.reply(message, mex);
+                            if (plant.length == 0) {
+                                bot.reply(message, "The machine is not responding");
+                                return;
+                            }
+                            console.log("textMach: ", textMach);
+                            bot.reply(message, textMach+"%");                          
+                             
+                        })
+             
+              
 
                     askForFurtherLines(plant, mpattern, controller, bot, message);
 
-                });
+               
 
             };
 
