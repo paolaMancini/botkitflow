c9dee1c7aa8e611595ccb1d972868aa0a60b1db3
paolaMancini <paola.mancini@italtel.com>
2018-02-22 10:18:48 +0100

    Update allPerformance.js

--- skills/allPerformance.js
+++ skills/allPerformance.js
@@ -23,29 +23,27 @@ module.exports = function(controller) {
                     console.log("Machines number: ", num);
 
                     var mex = "The performance values are:<br>";
-                    var aliasM; 
+                    var aliasM;
+                   
                     for (var i = 0; i < num; i++) {
-                        aliasM=plant.machines[i].alias;
-                        //Fetch performance value for every machine
-                        Events.fetchMachDetails(plant.machines[i].machine, function(errMach, events, textMach) {
+                        var mach = plant.machines[i].machine;
+
+                        var aliasM = plant.machines[i].alias;
+                    
+                        //Fetch availability value for every machine
+                        Events.fetchMachDetails1(mach,aliasM,"performance",function(errMach, events, textMach) {
                             if (errMach) {
-                                bot.reply(message, "*sorry, could not contact the organizers :-(*");
+                                bot.reply(message, "The machine is not responding");
                                 return;
                             }
 
-                            if (events.length == 0) {
-                                bot.reply(message, textMach + "\n\n_Type next for upcoming events_");
+                            if (plant.length == 0) {
+                                bot.reply(message,"The machine is not responding");
                                 return;
                             }
-
-                            for (var i = 0; i < events.machine.length; i++) {
-                                var current = events.machine[i];
-                                if (events.machine[i].name == "performance") {
-                                    mex +=  aliasM+ ": **" + current.value + "**%<br>";
-                                }
-                            }
-                            console.log("text: ", mex);
-                            bot.reply(message, mex);
+                            console.log("textMach: ", textMach);
+                            bot.reply(message, textMach);                          
+                             
                         })
 
                     }
