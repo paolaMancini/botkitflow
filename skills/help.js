923729e255bd256090abdcd0a73e12e554824f21
paolaMancini <paola.mancini@italtel.com>
2018-02-22 12:23:56 +0100

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,17 +5,17 @@ module.exports = function (controller) {
 
     controller.hears([/^help/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "performance data about plant <plant name>| performance data about all machines") + ": Ask to know the performance values about every line in the plant ";
-        text += "\n- " + bot.appendMention(message, "OEE data about plant <plant name>| OEE data about all machines") + ": Ask to know the OEE values about every line in the plant ";
-        text += "\n- " + bot.appendMention(message, "quality data about plant <plant name>| quality data about all machines") + ": Ask to know the quality values about every line in the plant ";
-        text += "\n- " + bot.appendMention(message, "availability data about plant <plant name>| availability data about all machines") + ": Ask to know the availability values about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "performance data about plant <plant name>| all performance values") + ": Ask to know the performance values about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "OEE data about plant <plant name>| all OEE values") + ": Ask to know the OEE values about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "quality data about plant <plant name>| all values") + ": Ask to know the quality values about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "availability data about plant <plant name>| all availability values") + ": Ask to know the availability values about every line in the plant ";
         text += "\n- " + bot.appendMention(message, "<machine name> details") + ": Ask to know all the values available for the spcific line";
         text += "\n- " + bot.appendMention(message, "line <line_name> quality|availability|OEE|okCounter|koCounter|goal|timeWork|performance|availability|oee|quality|timeActive|timeStop|okLastMinute|isStopped") + ": ask to know the specified value about a specific line";
         text += "\n- " + bot.appendMention(message, "line <line_name> availability") + ": ask to know the availability value about a specific line";
         text += "\n- " + bot.appendMention(message, "line <line_name> OEE") + ":  ask to know the OEE value about a specific line";
         text += "\n- " + bot.appendMention(message, "line <line_name> performance") + ": ask to know the performance value about the line";
-        text += "\n- " + bot.appendMention(message, "variables") + ": ask to know details about the values";
-    
+        text += "\n- " + bot.appendMention(message, "plants monitored") + ": ask to know details about the values";
+        text += "\n- " + bot.appendMention(message, "param descriptions") + ": ask to know details about the values";
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         bot.reply(message, text);
     });
