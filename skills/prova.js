4f51fda63269b966038b74608abfc68db288a378
paolaMancini <paola.mancini@italtel.com>
2018-02-22 05:39:15 +0100

    Create prova.js

---
+++ skills/prova.js
@@ -0,0 +1,13 @@
+module.exports = function (controller) {
+
+    controller.hears([/^prova$/], 'direct_message,direct_mention', function (bot, message) {
+
+        bot.startConversation(message, function (err, convo) {
+            bot.reply('This is a BotKit conversation sample.');
+            bot.reply ('- [Oh yea!](http://194.79.57.109:8080/SFnotify/mainChart)');
+               
+            });
+        });
+
+    });
+};
