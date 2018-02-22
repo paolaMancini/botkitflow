06385d0600cc02ea0ea6d9adb8c39e63dee8ce08
paolaMancini <paola.mancini@italtel.com>
2018-02-22 12:20:18 +0100

    Update allOEE.js

--- skills/allOEE.js
+++ skills/allOEE.js
@@ -1,99 +1,53 @@
 var request = require('request');
 var Events = require("./events");
 module.exports = function(controller) {
-   
-    controller.hears([/all oee/i], 'direct_message,direct_mention',
+
+   controller.hears([/all oee/i], 'direct_message,direct_mention',
         function(bot, message) {
 
             console.log('message: ', message);
              
-                Events.fetchMachines(function(err, events, text) {
+                Events.fetchMachines(function(err, plant, text) {
                     if (err) {
-                        bot.reply(message, "*sorry, could not contact the organizers :-(*");
+                        bot.reply(message, "The machine is not responding");
                         return;
                     }
 
-                    if (events.length == 0) {
-                        bot.reply(message, text + "\n\n_Type next for upcoming events_");
+                    if (plant.length == 0) {
+                    bot.reply(message, "The machine is not responding");
                         return;
                     }
 
-                    // Store events
-                    console.log("text: ", text);
-                      var oees = "<br>";
-                      var aliases;
-                    var mpattern = [];
-                    var detailMsg;
-                    for (var i = 0; i < events.machines.length; i++) {
-                        var machine = events.machines[i].machine;
+                    var num = plant.machines.length;
+                    console.log("Machines number: ", num);
+                    var mex = "The oee values are:<br>";
+                                     
 
-                        var alias = events.machines[i].alias;
-                        var oee = events.machines[i].oee;
-                        mpattern.push();
-                        aliases += "**" + alias + "**<br>";
-                        var currentMsg = alias + ": **" + oee + "**%;";
-                        oees += alias + ": **" + oee + "%**;<br>";
-                        detailMsg += alias + ": **line" + i + "** or **" + machine + " details**;<br>";
+                   for (var i = 0; i < num; i++) {
+                        var mach = plant.machines[i].machine;
+                        var aliasM = plant.machines[i].alias;
+                    
+                        //Fetch availability value for every machine
+                        Events.fetchMachDetails1(mach,aliasM,"oee",function(errMach, events, textMach) {
+                            if (errMach) {
+                                bot.reply(message, "The machine is not responding");
+                                return;
+                            }
+
+                            if (plant.length == 0) {
+                                bot.reply(message, "The machine is not responding");
+                                return;
+                            }
+                            console.log("textMach: ", textMach);
+                            bot.reply(message, textMach);                          
+                             
+                        })
+
                     }
-                    //mpattern.join(" ");
-                     
-                    bot.startConversation(message, function(err, convo) {
-                        // create a path for when a user says YES
-                        var help = "Which line are you interested of? Please, type:<br>";
-                        help += "**'machine' details**<br>";
-                        help +="Choose machine the name from the following list:";
-                        help +=aliases;
-                   
-
-                        convo.addMessage({
-                            text: `_${help}_`,
-                        }, 'ask-details');
-
-                        // create a path where neither option was matched
-                        // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
-                        convo.addMessage({
-                            text: 'Sorry I did not understand. Say `yes` or `no`',
-                            action: 'default',
-                        }, 'bad_response');
-
-
-                        convo.say("The OEE data is:" + oees);
-                        convo.ask("<br>Do you want furhter more details? (yes/**no**/cancel)<br>", [{
-                                pattern: "yes|yeh|sure|oui|si",
-                                callback: function(response, convo) {
-                                    convo.gotoThread('ask-details');
-                                },
-                            },
-                            {
-                                pattern: "no|neh|non|na|birk",
-                                callback: function(response, convo) {
-                                    convo.say("Glad have being helped you!");
-                                    convo.next();
-
-                                },
-                            },
-                            {
-                                pattern: "cancel|stop|exit",
-                                callback: function(response, convo) {
-                                    convo.say("Got it, cancelling...");
-                                    convo.next();
-                                },
-                            },
-                            {
-                                default: true,
-                                callback: function(response, convo) {
-                                    convo.say("Sorry, I did not understand.");
-                                    convo.repeat();
-                                    convo.next();
-                                }
-                            },
-                        ]);
-
-
-                    });
+  
                 });
             
 
-        })
+        });
 }
- 
+
