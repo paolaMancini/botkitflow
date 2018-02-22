15e6c1e2544da94d1957e164789671a1b4ecf485
paolaMancini <paola.mancini@italtel.com>
2018-02-22 13:02:07 +0100

    Update plantMonitored.js

--- skills/plantMonitored.js
+++ skills/plantMonitored.js
@@ -1,15 +1,8 @@
-module.exports = function (controller) {
+module.exports = function(controller) {
 
-    controller.hears(['plants monitored'], 'direct_message,direct_mention', function (bot, message) {
-
-        bot.startConversation(message, function (err, convo) {
-            convo.say('This is an example of using convo.ask with a single callback.');
-
-            convo.ask('What is your favorite color?', function (response, convo) {
-                convo.say('Cool, I like ' + response.text + ' too!');
-                convo.next();
-            });
-        });
-
+    controller.hears(['plants moitored'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+         bot.reply(message, "The plants monitored are:<br>**Plant1**");
     });
-};
+
+
+}
