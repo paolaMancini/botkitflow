b6b91943762a942e3b88eeab9d9021b842c29fab
paolaMancini <paola.mancini@italtel.com>
2018-02-22 08:23:11 +0100

    Update prova.js

--- skills/prova.js
+++ skills/prova.js
@@ -1,13 +1,13 @@
 module.exports = function (controller) {
 
-    controller.hears(['prova'], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^storage$/], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
             bot.reply('This is a BotKit conversation sample.');
-            bot.reply ('- [Oh yea!](http://194.79.57.109:8080/SFnotify/mainChart)');
+            bot.reply ("- [Oh yea!](http://194.79.57.109:8080/SFnotify/mainChart)");
                
             });
         });
 
-    });
+    
 };
