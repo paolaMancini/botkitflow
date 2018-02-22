3a9bc827d2b86fc4951c3b51a5584de0b0bf7020
paolaMancini <paola.mancini@italtel.com>
2018-02-22 09:09:55 +0100

    Update events.js

--- skills/events.js
+++ skills/events.js
@@ -105,4 +105,58 @@ module.exports.fetchMachDetails = function(machine, cb) {
         cb(null, events, msg);
     });
 }
+module.exports.fetchMachDetails1 = function(machine,alias, cb) {
+    var request = require("request");
+    // Get list of upcoming events
+    var options = {
+        method: 'GET',
+        url: "http://194.79.57.109:8080/SFapi/status?machine=" + machine
+    };
+
+    request(options, function(error, response, body) {
+        if (error) {
+            debug("1 could not retreive list of events, error: " + error);
+            //cb(new Error("Could not retreive current events, sorry [Events API not responding]"), null, null);
+            return;
+        }
+
+        if ((response < 200) || (response > 299)) {
+            debug("1 could not retreive list of events, response: " + response);
+            //sparkCallback(new Error("Could not retreive current events, sorry [bad anwser from Events API]"), null, null);
+            return;
+        }
+        
+        debug("body: ",body );
+        var events = JSON.parse(body);
+        //debug("fetched " + events.machine.length + " events");
+        //fine(JSON.stringify(events));
+
+        if (events.machine.length == 0) {
+            cb(null, events, "**Found no event currently going on.**");
+            return;
+        }
+
+        var nb = events.machine.length;
+        var msg = "<br>Nitty-gritty of the line:<br>";
+        if (nb == 1) {
+            msg = "No details found";
+        }
+        for (var i = 0; i < nb; i++) {
+            var current = events.machine[i];
+            //msg += "\n:small_blue_diamond: "
+            //msg += "\n" + (i + 1) + ". ";
+            if (i > 0) {
+                msg += "<br>";
+            }
+            //msg += current.machine + " - " + current.description + +" - " +  current.machine;
+            msg += current.name + ": **" + current.value + "**";
+            //debug("msg= ", msg);
+        }
+        msg+="<br>";
+
+
+
+        cb(null, events, msg);
+    });
+}
  
