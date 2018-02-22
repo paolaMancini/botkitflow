2052f08063f0dedb544548170f22a3a1c62dddaf
paolaMancini <paola.mancini@italtel.com>
2018-02-22 15:38:13 +0100

    Update prova.js

--- skills/prova.js
+++ skills/prova.js
@@ -1,9 +1,9 @@
  
 module.exports = function(controller) {
 
-    controller.hears(['prova'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears(['web portal'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         
-        var chosen_message = '[chart!](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine0&graph=2&graphPage=0)';
+        var chosen_message = '[ROLD SmartFab web portal!](http://194.79.57.109/smartFabDEMO/)';
         bot.reply(message, chosen_message);
         bot.reply();
 
