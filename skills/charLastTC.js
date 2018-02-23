825c2d5675a275385dd4caf6a1130600bee623d7
paolaMancini <paola.mancini@italtel.com>
2018-02-23 17:05:59 +0100

    Update charLastTC.js

--- skills/charLastTC.js
+++ skills/charLastTC.js
@@ -1,16 +1,14 @@
 module.exports = function(controller) {
 
-    controller.hears([/pieces done last tc/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        var message_options = [
-            "Fine thanks!",
-            "Very well, thank you.",
-            "Not so bad, thanks for asking.",
-            "Very good, thank you.",
-        ]
-        
-        var random_index = Math.floor(Math.random() * message_options.length)
-        var chosen_message = message_options[random_index];
-       
+    controller.hears(['pieces done last minute'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        var markdown='[:chart_with_upwards_trend:](http://194.79.57.109:8080/SFnoify/mainChart)';
+        var chosen_message = "Click on the icon to see the trend "+markdown;
+      
         bot.reply(message, chosen_message);
-     });
+        bot.reply();
+
+    });
+
+
 }
+
