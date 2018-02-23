0172919d3e218cd80f68967dfaf967dd6a644ef3
paolaMancini <paola.mancini@italtel.com>
2018-02-23 16:51:53 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,20 +1,4 @@
-
-module.exports = function(controller) {
-
-    controller.hears([/pieces done last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        var message_options = [
-            "Fine thanks!",
-            "Very well, thank you.",
-            "Not so bad, thanks for asking.",
-            "Very good, thank you.",
-        ]
-        
-        var random_index = Math.floor(Math.random() * message_options.length)
-        var chosen_message = message_options[random_index];
-       
-        bot.reply(message, chosen_message);
-     });
-}
+ 
 module.exports = function(controller) {
 
     controller.hears(['pieces done last minute'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
