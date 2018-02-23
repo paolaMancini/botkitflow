576e648a387f8301029f49dc227da3d9698e34e6
paolaMancini <paola.mancini@italtel.com>
2018-02-23 17:01:58 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,7 +1,7 @@
  
 module.exports = function(controller) {
 
-    controller.hears(['pieces done last minute'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears(['last minute'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
       //var markdown='[ROLD SmartFab web portal!](http://194.79.57.109/smartFabDEMO/)';
         var markdown='[:bar_chart:](http://194.79.57.109:8080/SFnotify/mainChart)';
         var chosen_message = "Click on the icon to see the trend "+markdown;
