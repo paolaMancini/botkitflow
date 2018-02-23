9c5196044c6d4b746592137a808d48be1829bef3
paolaMancini <paola.mancini@italtel.com>
2018-02-23 18:26:58 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,15 +1,33 @@
  
-module.exports = function(controller) {
+  var request = require("request");
+ var url = require('url');
 
-    controller.hears(['last minute']/i, ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-      //var markdown='[ROLD SmartFab web portal!](http://194.79.57.109/smartFabDEMO/)';
-        var markdown='[:bar_chart:](http://194.79.57.109:8080/SFnotify/mainChart)';
-        var chosen_message = "Click on the icon to see the trend "+markdown;
-      
-        bot.reply(message, chosen_message);
-        bot.reply();
+ module.exports = function(controller) {
 
-    });
+     controller.hears(['last minute'] / i, ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+
+         var adr = 'http: //194.79.57.109:8080/SFnotify/mainChart';
+         var q = url.parse(adr, true);
+         var options = {
+             method: 'POST',
+             url: 'https://api.ciscospark.com/v1/messages',
+             headers: {
+                 authorization: 'Bearer YjJiOTEwNmItYzUzMC00MzlmLTgxOWUtMGNjYWFlZWZkMWQ5MjI2MzMxZjMtMmRi',
+                 'content-type': 'multipart/form-data'
+             },
+             formData: {
+                 roomId: 'Y2lzY29zcGFyazovL3VzL1JPT00vMzI3ZThhMjAtMTYxOC0xMWU4LTlhODEtYzFhMjhlNzQzYjQz',
+                 text: 'This is a message with file attachment',
+                 files: q
+             }
+         };
+
+         request(options, function(error, response, body) {
+             if (error) throw new Error(error);
+
+             console.log(body);
+         });
 
 
-}
+     });
+ }
