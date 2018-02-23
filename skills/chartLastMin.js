f2fb078933a0896a1a9b2db188913599b3e30d6e
paolaMancini <paola.mancini@italtel.com>
2018-02-23 16:51:40 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -15,3 +15,16 @@ module.exports = function(controller) {
         bot.reply(message, chosen_message);
      });
 }
+module.exports = function(controller) {
+
+    controller.hears(['pieces done last minute'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        var markdown='[:bar_chart:](http://194.79.57.109:8080/SFnoify/mainChart)';
+        var chosen_message = "Click on the icon to see the trend "+markdown;
+      
+        bot.reply(message, chosen_message);
+        bot.reply();
+
+    });
+
+
+}
