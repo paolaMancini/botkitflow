50462e3031e77ab51f7c79067c31c41384f3e3a4
paolaMancini <paola.mancini@italtel.com>
2018-02-23 18:52:12 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -6,8 +6,8 @@
 
      controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
 
-         var adr = 'http: //194.79.57.109:8080/SFnotify/mainChart';
-         var q = url.parse(adr, true);
+         v var adr  decodeURI("http: //194.79.57.109:8080/SFnotify/mainChart");
+
          var options = {
              method: 'POST',
              url: 'https://api.ciscospark.com/v1/messages',
@@ -18,7 +18,7 @@
              formData: {
                  roomId: 'Y2lzY29zcGFyazovL3VzL1JPT00vMzI3ZThhMjAtMTYxOC0xMWU4LTlhODEtYzFhMjhlNzQzYjQz',
                  text: 'This is a message with file attachment',
-                 files: q
+                 files: adr
              }
          };
 
