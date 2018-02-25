8650adfd24209dae54f8603e4794e88f4d284080
paolaMancini <paola.mancini@italtel.com>
2018-02-25 04:22:16 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -6,7 +6,7 @@
 
      controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
 
-         var adr = decodeURI("http://194.79.57.109:8080/SFnotify/mainChart");
+         var uri = new Uri('http: //194.79.57.109:8080/SFnotify/mainChart');
 
          var options = {
              method: 'POST',
@@ -18,7 +18,7 @@
              formData: {
                  roomId: 'Y2lzY29zcGFyazovL3VzL1JPT00vNzljY2QyMTAtMDQ5My0xMWU4LTlhZjktZTczYzkzNDQyNGNk',
                  text: 'This is a message with file attachment',
-                 files: adr
+                 files: uri
              }
          };
 
