93f91bc86dfbe66abeda443f0a2d8357340abb61
paolaMancini <paola.mancini@italtel.com>
2018-02-25 07:19:52 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,17 +1,9 @@
- 
- 
-var request = require("request");  
-var url = require("url");
 module.exports = function(controller) {
 
     controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
     console.log("message: ",message.raw_message.data.roomId);
-    var roomId=
-
-     var urlValue = 'http://194.79.57.109:8080/SFnotify/mainChart';
-     var parsedUrl = url.parse(urlValue, true, true);
-          
-     
+    
+  
      controller.hears('another_keyword','direct_message,direct_mention',function(bot,message) {
   var reply_with_attachments = {
     'username': 'My bot' ,
