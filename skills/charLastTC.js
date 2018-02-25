6f2b83ece0c5a5e5421c47f69989c2f54f11183b
paolaMancini <paola.mancini@italtel.com>
2018-02-25 06:59:41 +0100

    Update charLastTC.js

--- skills/charLastTC.js
+++ skills/charLastTC.js
@@ -1,8 +1,8 @@
 module.exports = function(controller) {
 
-    controller.hears([/'time cicle'|tc/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears([/time cicle|tc/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         
-        var markdown0='[machine_0!](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine0&graph=2&graphPage=0)';
+        var markdown0='[machine_0](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine0&graph=2&graphPage=0)';
         var markdown1='[machine_1](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine1&graph=2&graphPage=0)';
         var markdown2='[machine_2](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine2&graph=2&graphPage=0)';
         var markdown3='[machine_3](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine3&graph=2&graphPage=0)';
