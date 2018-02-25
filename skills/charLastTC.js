e0f18e7b265bc84038cbf742fd357cdce834eaf6
paolaMancini <paola.mancini@italtel.com>
2018-02-25 06:36:16 +0100

    Update charLastTC.js

--- skills/charLastTC.js
+++ skills/charLastTC.js
@@ -1,26 +1,24 @@
 module.exports = function(controller) {
 
     controller.hears([/last tc/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        bot.say("roomId= "+message.raw_message.roomId);
         
-        //message.raw_message.actorId;
+        var markdown0='![machine_0](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine0&graph=2&graphPage=0)';
+        var markdown1='![machine_1](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine1&graph=2&graphPage=0)';
+        var markdown2='![machine_2](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine2&graph=2&graphPage=0)';
+        var markdown3='![machine_3](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine3&graph=2&graphPage=0)';
+        var markdown4='![machine_4](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine4&graph=2&graphPage=0)';
+        var markdown5='![machine_5](http://194.79.57.109:8080/SFnotify/chart?machine=fakeMachine5&graph=2&graphPage=0)';
+        var markdown="* "+markdown0+"<br>* "+markdown1+"<br>* "+markdown2+"<br>* "+markdown3+"<br>* "+markdown4+"<br>* "+markdown5;
+       var chosen_message = "Pieces done in in the last Time cicle:<br> "+markdown;
+      
+        bot.reply(message, chosen_message);
+        bot.reply();
+
+    });
        
 }
 
-function getMachineName(alias) {
-    if (alias=="machine_0"){
-    	return "fakeMachine0";
-    }else if (alias=="machine_1"){
-   		 return "fakeMachine1";
-    }else if (alias=="machine_2"){
-    	return "fakeMachine2";
-    }else if (alias=="machine_3"){
-    	return "fakeMachine3";
-    }else if (alias=="machine_4"){
-   		 return "fakeMachine4";
-    }else{
-    	//alias=="machine_4"
-        return "fakeMachine5";
-    }                
-}
+
 
  
