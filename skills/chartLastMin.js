859c5e96767d97c40fb9320e50efeea886496798
paolaMancini <paola.mancini@italtel.com>
2018-02-25 07:33:49 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,39 +1,19 @@
 module.exports = function(controller) {
+controller.hears(['web portal'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        var markdown='[ROLD SmartFab Chart!](http://194.79.57.109:8080/SFnoify/mainChart)';
+        var chosen_message = "Go to "+markdown+" to view the pieces done in the plant  in the last miute "+markdown;
+        console.log (message);
+        bot.reply(message, chosen_message);
+        bot.reply();
 
-    controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-    console.log("message: ",message.raw_message.data.roomId);
-    
-  
-     controller.hears('another_keyword','direct_message,direct_mention',function(bot,message) {
-  var reply_with_attachments = {
-    'username': 'My bot' ,
-    'text': 'This is a pre-text',
-    'attachments': [
-      {
-        'fallback': 'To be useful, I need your to invite me in a channel.',
-        'title': 'How can I help you?',
-        'text': 'To be useful, I need your to invite me in a channel ',
-        'color': '#7CD197'
-      }
-    ],
-    'icon_url': 'http://194.79.57.109:8080/SFnotify/mainChart'
-    }
-
-  bot.reply(message, reply_with_attachments);
-});
-     
-     
-        
-
-     });
+    });
  }
 /**var request = require("request");  
 var url = require("url");
 module.exports = function(controller) {
 
     controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-    car roomId= 
-
+     
      var urlValue = 'http://194.79.57.109:8080/SFnotify/mainChart';
      var parsedUrl = url.parse(urlValue, true, true);
           
