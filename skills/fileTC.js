b638a14307322ef329e0144643f4ca41a34f9f13
paolaMancini <paola.mancini@italtel.com>
2018-03-02 16:35:11 +0100

    Update fileTC.js

--- skills/fileTC.js
+++ skills/fileTC.js
@@ -1,64 +1,68 @@
-<script>
-//
-// Displays the code of the specified skill
-//
-module.exports = function (controller) {
+module.exports = function(controller) {
 
-    controller.hears([ [/pao cicle|pc/i], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears(
+        [/time cicle|tc/i], 'direct_message,direct_mention',
+        function(bot, message) {
 
-        // Fetch value argument
-        
+            // Fetch value argument
 
-        bot.startConversation(message, function (err, convo) {
 
-            convo.ask("Please choose a machine among 'machine_0', 'machine_1', 'machine_2', 'machine_3', 'machine_4', 'machine_5'", [
-                {
-                    pattern: "^machine_0|machine_1|machine_2|machine_3|machine_5$",
-                    callback: function (response, convo) {
-                        // ends current conversation
-                        convo.stop();
-    
-                        showMachine(fromAliasToName(response.text), bot, message);
-                        return;
+            bot.startConversation(message, function(err, convo) {
+
+                convo.ask("Please choose a machine among 'machine_0', 'machine_1', 'machine_2', 'machine_3', 'machine_4', 'machine_5'", [{
+                        pattern: "^machine_0|machine_1|machine_2|machine_3|machine_5$",
+                        callback: function(response, convo) {
+                            // ends current conversation
+                            convo.stop();
+
+                            showMachine(fromAliasToName(response.text), bot, message);
+                            return;
+                        },
                     },
-                },
-                {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.say("Sorry, this machine is not correct. Try again...");
-                        convo.repeat();
-                        convo.next();
+                    {
+                        default: true,
+                        callback: function(response, convo) {
+                            convo.say("Sorry, this machine is not correct. Try again...");
+                            convo.repeat();
+                            convo.next();
+                        }
                     }
-                }
-            ]);
-        });
-    })
+                ]);
+            });
+        })
 }
+
 function showMachine(machine, bot, message) {
     // Append .js extension
-    console.log ('machine: '+machine);
-    if(machine==undefined){
-        bot.reply(message,"Sorry, this machine is not correct.");    
-    }else{
-        bot.reply(message,"In the chart the requested data");
-        var link base='http://194.79.57.109:8080/SFnotify/chart?machine=');
-        var link='http://194.79.57.109:8080/SFnotify/chart?machine=',machine,'5&graph=2&graphPage=0');
-        bot.reply(message,{text:'Here is your file!', files:[link]});
-    });
- };
- 
-function fromAliasToName(p1){
-	if (p1=="machine_0"){
-    	return "fakeMachine0";
-    }else if (p1=="machine_1"){
-    	return "fakeMachine1";
-    }else if (p1=="machine_2"){
-    	return "fakeMachine2";
-    }else if (p1=="machine3"){
-    	return "fakeMachine3";
-    }else if (p1=="machine_4"){
-    	return "fakeMachine4";
-    }else if (p1=="machine_5"){
-    	return "fakeMachine5";
-    } 
+    console.log('machine: ' + machine);
+    if (machine == undefined) {
+        bot.reply(message, "Sorry, this machine is not correct.");
+    } else {
+        bot.reply(message, "In the chart the requested data");
+
+        //Query string is now: 'foo=1&bar=2&foo=4'
+        let urlBegin = 'http://194.79.57.109:8080/SFnotify/chart?machine=5&graph=2&graphPage=0';
+        let urlEnd = '5&graph=2&graphPage=0';
+
+        var s2 = new String(urlBegin + machine + urlEnd)
+
+        bot.reply(message, { text: 'Here is your file!', files: [link] });
+    }
+};
+
+
+function fromAliasToName(p1) {
+    if (p1 == "machine_0") {
+        return "fakeMachine0";
+    } else if (p1 == "machine_1") {
+        return "fakeMachine1";
+    } else if (p1 == "machine_2") {
+        return "fakeMachine2";
+    } else if (p1 == "machine3") {
+        return "fakeMachine3";
+    } else if (p1 == "machine_4") {
+        return "fakeMachine4";
+    } else if (p1 == "machine_5") {
+        return "fakeMachine5";
+    }
 }
