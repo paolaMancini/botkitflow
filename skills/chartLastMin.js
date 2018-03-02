64d9a72875eff408ddd0f1ddca02436a427a1cef
paolaMancini <paola.mancini@italtel.com>
2018-03-02 14:53:57 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,14 +1,15 @@
 var url = require("url");
 module.exports = function(controller) {
 controller.hears(['last minute'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        var markdown='[Chart](http://194.79.57.109:8080/SFnotify/mainChart)';
-        var chosen_message = "Click on "+markdown+" to obtain the requested data";
+        //var markdown='[Chart](http://194.79.57.109:8080/SFnotify/mainChart)';
+        //var chosen_message = "Click on "+markdown+" to obtain the requested data";
         console.log (message);
+        var chosen_message ="In the chart the requested data";
         bot.reply(message, chosen_message);
         //var urlValue = 'http://194.79.57.109:8080/SFnotify/mainChart';
-        var urlValue = 'http://cloudapps.italtel.com/December.jpg';
-        var parsedUrl = url.parse(urlValue, true, true);
-       bot.reply(message,{text: 'I made this file for you.', files:['http://194.79.57.109:8080/SFnotify/mainChart'
+        //var urlValue = 'http://cloudapps.italtel.com/December.jpg';
+        //var parsedUrl = url.parse(urlValue, true, true);
+        bot.reply(message,{text: '', files:['http://194.79.57.109:8080/SFnotify/mainChart'
 ]});
      });
  }
