062bb50440b1302ec30ea201851026a4cf1080e5
paolaMancini <paola.mancini@italtel.com>
2018-03-02 14:07:51 +0100

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,3 +1,4 @@
+var url = require("url");
 module.exports = function(controller) {
 controller.hears(['last minute'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
         var markdown='[Chart](http://194.79.57.109:8080/SFnotify/mainChart)';
