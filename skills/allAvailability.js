58c74c88a12b230f7c49d69aa365fd32fe15a9fa
paolaMancini <paola.mancini@italtel.com>
2018-06-04 13:50:57 +0200

    Update allAvailability.js

--- skills/allAvailability.js
+++ skills/allAvailability.js
@@ -2,7 +2,7 @@ var request = require('request');
 var Events = require("./events");
 module.exports = function(controller) {
 
-    controller.hears([/all availability/i], 'direct_message,direct_mention',
+    controller.hears([/availabilities/i], 'direct_message,direct_mention',
         function(bot, message) {
 
             console.log('message: ', message);
