ab5d904f91966f0398921fa19ad4520b5f88b338
paolaMancini <paola.mancini@italtel.com>
2018-06-05 10:59:49 +0200

    Update chartLastMin.js

--- skills/chartLastMin.js
+++ skills/chartLastMin.js
@@ -1,17 +1,44 @@
 
+var url = require("url");
 module.exports = function(controller) {
-
-    controller.hears([/pieces done last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-        var message_options = [
-            "Fine thanks!",
-            "Very well, thank you.",
-            "Not so bad, thanks for asking.",
-            "Very good, thank you.",
-        ]
-        
-        var random_index = Math.floor(Math.random() * message_options.length)
-        var chosen_message = message_options[random_index];
-       
+controller.hears(['last minute'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+        //var markdown='[Chart](http://194.79.57.109:8080/SFnotify/mainChart)';
+        //var chosen_message = "Click on "+markdown+" to obtain the requested data";
+        console.log (message);
+        var chosen_message ="In the chart the requested data";
         bot.reply(message, chosen_message);
+        //var urlValue = 'http://194.79.57.109:8080/SFnotify/mainChart';
+        //var urlValue = 'http://cloudapps.italtel.com/December.jpg';
+        //var parsedUrl = url.parse(urlValue, true, true);
+        bot.reply(message,{text: '', files:['http://194.79.57.109:8080/SFnotify/mainChart'
+]});
      });
-}
+ }
+/**var request = require("request");  
+var url = require("url");
+module.exports = function(controller) {
+    controller.hears([/last minute/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+     
+     var urlValue = 'http://194.79.57.109:8080/SFnotify/mainChart';
+     var parsedUrl = url.parse(urlValue, true, true);
+          
+         var options = {
+             method: 'POST',
+             url: 'https://api.ciscospark.com/v1/messages',
+             headers: {
+                 authorization: 'Bearer YjJiOTEwNmItYzUzMC00MzlmLTgxOWUtMGNjYWFlZWZkMWQ5MjI2MzMxZjMtMmRi',
+                 'content-type': 'multipart/form-data'
+             },
+             formData: {
+                 roomId:  message.raw_message.data.roomId,
+                 text: 'This is a message with file attachment',
+                 files: parsedUrl
+             }
+         };
+         request(options, function(error, response, body) {
+             if (error) throw new Error(error);
+             console.log(body);
+         });
+     });
+ }**/
+ 
