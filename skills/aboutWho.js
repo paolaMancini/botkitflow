bb9772d821993b01172dd4f51693c67b0a1821f8
paolaMancini <paola.mancini@italtel.com>
2018-06-11 11:52:58 +0200

    Update aboutWho.js

--- skills/aboutWho.js
+++ skills/aboutWho.js
@@ -4,7 +4,7 @@
 module.exports = function(controller) {
 
     controller.hears(['who are you'], 'direct_message,direct_mention', function(bot, message) {
-        var mardown = "This is the IndychatBot for ROLD SmartFab, the ready-to-use platform which allows the SMEs to monitor, analyze and manage data and information coming from production plants. To know which information I provide, please type:<br>";
+        var mardown = "This is the IndychatBot for ROLD SmartFab, the ready-to-use platform which allows the SMEs to monitor, analyze and manage data and information coming from production plants.";
         mardown += bot.appendMention(message, "help");
 
         bot.reply(message, mardown);
