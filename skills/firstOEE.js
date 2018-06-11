3347b4eebc6b0f111552ebb3901642e1ac252788
paolaMancini <paola.mancini@italtel.com>
2018-06-11 09:28:43 +0200

    Update firstOEE.js

--- skills/firstOEE.js
+++ skills/firstOEE.js
@@ -4,7 +4,7 @@ var Events = require("./events");
 module.exports = function(controller) {
 
     //controller.hears([/availability value about line (.*)/i], 'direct_message,direct_mention', function(bot, message) {
-    controller.hears([/line (.*) oee/i], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears([/oee  value for (.*)/i], 'direct_message,direct_mention', function(bot, message) {
         console.log('message: ', message);
         var lineName = message.match[1];
         var param = "oee";
