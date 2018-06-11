9ecc963fc11f1f8325708d927ad599eeb675b7c2
paolaMancini <paola.mancini@italtel.com>
2018-06-11 16:44:29 +0200

    Update linesMonitored.js

--- skills/linesMonitored.js
+++ skills/linesMonitored.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
 
     controller.hears(['machines'], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
-         bot.reply(message, "Monitored machines: <br>**machine_0**,**machine_1**, **machine_2**, **machine_3**, **machine_4**, **machine_5**");
+         bot.reply(message, "Monitored machines: <br>**machine0**,**machine1**, **machine2**, **machine3**, **machine4**, **machine5**");
     });
 
 
