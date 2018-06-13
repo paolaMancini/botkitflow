86668817f670232867fe72d06a1711ed3d88f2a7
paolaMancini <paola.mancini@italtel.com>
2018-06-13 10:29:22 +0200

    Update aboutWho.js

--- skills/aboutWho.js
+++ skills/aboutWho.js
@@ -5,7 +5,7 @@ module.exports = function(controller) {
 
     controller.hears(['who are you'], 'direct_message,direct_mention', function(bot, message) {
         var mardown = "This is the IndychatBot for ROLD SmartFab, the ready-to-use platform which allows the SMEs to monitor, analyze and manage data and information coming from production plants.";
-        mardown += bot.appendMention(message, "help");
+        //mardown += bot.appendMention(message, "help");
 
         bot.reply(message, mardown);
     });
